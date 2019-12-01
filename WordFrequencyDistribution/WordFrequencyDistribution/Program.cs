4a12f40c49bd544bdee1cd2d848f84681b7cbb9b
calabriamo <a1687257@student.adelaide.edu.au>
2019-12-01 21:13:32 +1030

    Solution file fix

---
+++ WordFrequencyDistribution/WordFrequencyDistribution/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFrequencyDistribution
+{
+    class Program
+    {
+        static private string _filePath = "C:\\Users\\Jack\\Desktop\\";
+        static private string _fileName = "test.txt";
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting program");
+
+            TextFileReader.ReadFile(_filePath + _fileName);
+
+            Tokeniser.Tokenise(TextFileReader._fileTextLines);
+            Tokeniser.PrintTokens();
+
+            // Pauses the console so console-printed messages are perceptible.
+            Console.ReadKey();
+        }
+    }
+}
